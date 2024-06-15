af2949f02f6878a9bd8e177091cd200e852a37d1
Kirill Komlev <kirillkomlev02@gmail.com>
2024-06-15 19:55:06 +0300

    Updates

---
+++ assets/Products-DynjsyQL.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D5Glcd-r.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("main",{className:"content",children:"Products"})})}export{n as default};
--- assets/Products-WoG6mXKx.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BdGsBGtz.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("main",{className:"content",children:"Products"})})}export{n as default};
