ee710d74b5297a3eff4a3930c71abb973323d000
Kirill Komlev <kirillkomlev02@gmail.com>
2024-06-15 20:21:25 +0300

    Updates

---
+++ assets/Products-BJbbhrzl.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1J8jzbbN.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("main",{className:"content",children:"Products"})})}export{n as default};
--- assets/Products-CnR0qBGD.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DeIDr9yQ.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("main",{className:"content",children:"Products"})})}export{n as default};
